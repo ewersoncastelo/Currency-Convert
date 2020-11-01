2cb9b29f0f5bc4b88c1a271474742ec419f6e733
Ewerson Castelo <ewerson.castelo@gmail.com>
2020-11-01 11:27:37 -0400

    add infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,18 @@
+//
+//  AlamofireAdapterTests.swift
+//  AlamofireAdapterTests
+//
+//  Created by ewerson castelo on 01/11/20.
+//  Copyright © 2020 ewerson castelo. All rights reserved.
+//
+
+import XCTest
+
+// MARK: - Adapter is design pattern for adpating two interfaces distincts
+class AlamofireAdapterTests: XCTestCase {
+
+    func test_() throws {
+       
+    }
+
+}
